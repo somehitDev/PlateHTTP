2d20286ee145df88a843414fb4468a77845330f7
oyajiDev <this.dev.somehit@gmail.com>
2024-12-15 22:07:50 +0900

    Changes based on WebApplication changes

--- Extensions/Templating/Extends.cs
+++ Extensions/Templating/Extends.cs
@@ -1,18 +1,22 @@
 using System.Threading.Tasks;
-using PlateHTTP.Core;
+using PlateHTTP.Interfaces.Core;
 using PlateHTTP.Enums;
 
 
 
 namespace PlateHTTP.Extensions.Templating {
     public static class TemplatingExtends {
-        public static void UseTemplateLoader(this WebApplication webApplication, AbstractTemplateLoader templateLoader) {
-            webApplication.TemplateLoader = templateLoader;
+        public static void UseTemplateLoader(this IWebApplication webApplication, AbstractTemplateLoader templateLoader) {
+            webApplication.Extensions["Templating"] = templateLoader;
         }
 
-        public static async Task RenderTemplate(this Response response, string templateName, object? context = null, StatusCode statusCode = StatusCode.OK) {
-            if (response.WebApplication.TemplateLoader != null) {
-                var loader = (AbstractTemplateLoader)response.WebApplication.TemplateLoader;
+        public static async Task RenderTemplate(this IResponse response, string templateName, object? context = null, StatusCode statusCode = StatusCode.OK) {
+            if (!response.WebApplication.Extensions.ContainsKey("Templating")) {
+                response.WebApplication.Logger?.Error("`RenderTemplate` only use after map template by `UseTemplateLoader`!");
+                await response.SendText("404 Not Found!", StatusCode.NotFound);
+            }
+            else {
+                var loader = (AbstractTemplateLoader)response.WebApplication.Extensions["Templating"];
 
                 var template = Scriban.Template.Parse(
                     await loader.LoadAsync(loader.GetPath(templateName))
@@ -24,14 +28,14 @@ namespace PlateHTTP.Extensions.Templating {
 
                 await response.SendHTML(await template.RenderAsync(ctx), statusCode);
             }
-            else {
-                response.WebApplication.Logger?.Error("`RenderTemplate` only use after map template by `UseTemplateLoader`!");
-                await response.SendText("404 Not Found!");
-            }
         }
-        public static async Task RenderHTML(this Response response, string html, object? context = null, StatusCode statusCode = StatusCode.OK) {
-            if (response.WebApplication.TemplateLoader != null) {
-                var loader = (AbstractTemplateLoader)response.WebApplication.TemplateLoader;
+        public static async Task RenderHTML(this IResponse response, string html, object? context = null, StatusCode statusCode = StatusCode.OK) {
+            if (!response.WebApplication.Extensions.ContainsKey("Templating")) {
+                response.WebApplication.Logger?.Error("`RenderHTML` only use after map template by `UseTemplateLoader`!");
+                await response.SendText("404 Not Found!", StatusCode.NotFound);
+            }
+            else {
+                var loader = (AbstractTemplateLoader)response.WebApplication.Extensions["Templating"];
 
                 var template = Scriban.Template.Parse(html);
 
@@ -40,10 +44,6 @@ namespace PlateHTTP.Extensions.Templating {
                 ctx.TemplateLoader = loader;
 
                 await response.SendHTML(await template.RenderAsync(ctx), statusCode);
-            }
-            else {
-                response.WebApplication.Logger?.Error("`RenderHTML` only use after map template by `UseTemplateLoader`!");
-                await response.SendText("404 Not Found!");
             }
         }
     }
